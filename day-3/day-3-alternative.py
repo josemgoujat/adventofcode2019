22ca1ec1cafc037dff8e83733f4ebdcdfb07e4e6
josuco <josegoujat@gmail.com>
2019-12-14 16:57:59 +0100

    day 3 with simple list of tuples

--- day-3/day-3-alternative.py
+++ day-3/day-3-alternative.py
@@ -1,8 +1,13 @@
 """
 https://adventofcode.com/2019/day/3
 
-Simple approach, using a list of tuples (x, y) as path coordinates representation.
-More clear but slightly slower than alternative using dictionaries (see day-3-alternative.py).
+Alternative approach using dictionaries as data structure for path coordinates:
+{
+    x1: {y1: True, y2: True},
+    x2: {y1: True, y3: True, y4: True}
+}
+Slightly faster, probably because of doing an extra intersection that discards non shared x values
+between the two paths.
 """
 with open('day-3.input', 'r') as input_f:
     path_steps_1, path_steps_2 = [line.split(',') for line in input_f.read().splitlines()]
@@ -13,22 +18,29 @@ def parse_step(step):
 
 
 def move_horizontally(path_coordinates, x, y, length):
-    for i in range(x, x + length, 1 if length > 0 else -1):
-        path_coordinates.append((i, y))
+    _range = range(x, x + length, 1 if length > 0 else -1)
+    for i in _range:
+        if path_coordinates.get(i):
+            path_coordinates[i].update({y: True})
+        else:
+            path_coordinates[i] = {y: True}
 
     return path_coordinates, x + length
 
 
 def move_vertically(path_coordinates, x, y, length):
-    for j in range(y, y + length, 1 if length > 0 else -1):
-        path_coordinates.append((x, j))
+    _range = range(y, y + length, 1 if length > 0 else -1)
+    if path_coordinates.get(x):
+        path_coordinates[x].update({j: True for j in _range})
+    else:
+        path_coordinates[x] = {j: True for j in _range}
 
     return path_coordinates, y + length
 
 
 def build_path_coordinates(path_steps):
     x, y = 0, 0
-    path_coordinates = []
+    path_coordinates = {}
     for step in path_steps:
         direction, length = parse_step(step)
         if direction in 'RL':
@@ -40,8 +52,12 @@ def build_path_coordinates(path_steps):
 
 
 def get_intersections(path_coordinates_1, path_coordinates_2):
-    intersections = set(path_coordinates_1) & set(path_coordinates_2)
-    intersections.remove((0, 0))
+    intersections = []
+    for x in set(path_coordinates_1.keys()) & set(path_coordinates_2.keys()):
+        for y in set(path_coordinates_1[x].keys()) & set(path_coordinates_2[x].keys()):
+            if not (x == 0 and y == 0):
+                intersections.append((x, y))
+
     return intersections
 
 
